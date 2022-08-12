216f6a2b6b9b30ee8a48278fe4e0b26684f674c5
Josue Lobo <josuelobo2000@gmail.com>
2022-08-12 19:34:59 -0300

    Fim módulo 3

--- src/Fluxo.java
+++ src/Fluxo.java
@@ -1,7 +1,12 @@
 public class Fluxo {
     public static void main(String[] args) {
         System.out.println("Ini do main");
+        try{
         metodo1();
+        } catch(ArithmeticException | NullPointerException ex){
+            String msg = ex.getMessage();
+            System.out.println("Erro Aritimetico " + msg);
+        }
         System.out.println("Fim do main");
     }
 
@@ -15,6 +20,8 @@ public class Fluxo {
         System.out.println("Ini do metodo2");
         for(int i = 1; i <= 5; i++) {
             System.out.println(i);
+            
+            throw new NullPointerException("Deu erro");
         }
         System.out.println("Fim do metodo2");
     }
